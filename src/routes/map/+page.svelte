39057eee5e33925a16bb38372660594592b63ea7
mattsoza <mattsoza@protonmail.com>
2023-02-25 17:42:03 -0500

    Added 'under construction' to map link

---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,8 @@
+<!-- Scripting -->
+<script>
+    import coveredToilet from '../../assets/coveredToilet.png'
+</script>
+  
+<!-- HTML -->
+<h1>This page is under construction</h1>
+<img src="{coveredToilet}" width=700 alt="The inside of a stall bathroom. The toilet in the stall is covered up with a plastic black bag because the toilet is damaged or otherwise out of order." />
