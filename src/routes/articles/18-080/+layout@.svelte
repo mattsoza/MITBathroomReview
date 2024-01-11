e393d824c65da6cf7460f6dda804d3708e7a1117
mattsoza <mattsoza@protonmail.com>
2024-01-11 02:02:39 -0500

    Wrote 18-080 article. Fixed styling with elements added

--- src/routes/articles/18-080/+layout@.svelte
+++ src/routes/articles/18-080/+layout@.svelte
@@ -2,11 +2,21 @@
 <script>
 	import BathroomFooter from "/src/components/bathroomFooter.svelte";
   import Bathroomheader from "/src/components/RetroComponents/bathroomheaderRetro.svelte";
+
+  const documentHeight = document.documentElement.scrollHeight;
+
+  let bodyHeight = 0;
+
+  const getBodyHeight = (body) => {
+    bodyHeight = body.clientHeight;
+  };
 </script>
 
 
 
 <!-- HTML of nav and stylesheet -->
+<svelte:body use:getBodyHeight />
+<div class='vintage' style='height: {bodyHeight}px;'/>
 <Bathroomheader />
 <slot />
 <BathroomFooter />
@@ -19,18 +29,23 @@
 	font-weight: 600;
 	font-style: normal;
 	margin: 0;
-
-  background: linear-gradient(to right, #711DB0 1.5%, #C21292 1.5% 3%, #EF4040 3% 4.5%, #FFA732 4.5% 6%, #F2ECBE 6%);
   min-height: 100vh;
 }
 
-@media(max-width: 800px) {
-  :global(body) {
-    background: linear-gradient(180deg, #711DB0 45px, #C21292 45px 90px, #EF4040 90px 135px, #FFA732 135px 180px, #F2ECBE 180px);
-  }
+.vintage {
+  height: 100vh;
+  width: 100vw;
+  position: fixed;
+  z-index: -3;
+  background: linear-gradient(to right, #711DB0 1.5%, #C21292 1.5% 3%, #EF4040 3% 4.5%, #FFA732 4.5% 6%, #F2ECBE 6%);
 }
 
-
+@media(max-width: 800px) {
+  .vintage {
+    background: linear-gradient(#711DB0 45px, #C21292 45px 90px, #EF4040 90px 135px, #FFA732 135px 180px, #F2ECBE 180px);
+    position: absolute;
+  }
+}
 
 :global(html, body) { 
   min-height: 100vh;
---
+++ src/routes/articles/18-080/+page.svelte
@@ -0,0 +1,62 @@
+<script>
+  const title = '18-080'
+</script>
+
+<main>
+  <article>
+    <h1>{ title }</h1>
+    <p>Published: January 10, 2024</p>
+
+    <p>A bathroom is a reflection of the time it was built. Today, I visited bathroom 18-080, a restroom constructed in the 1970s.</p>
+
+    <p>Now, faithful reader, I will not jerk you around: Building 18 was remodeled in 2003. I unfortunately did not know this before spending many hours on the design of this webpage, and if you’ve ever heard of the sunk-cost fallacy, something important to know is that I am a victim of it and will not stop. So let’s press on and feign ignorance about the redesign*.</p>
+
+    <p>This bathroom is a must-see! The first thing I ran into was multiple chairs in the restroom. Now this I respect and enjoy immensely. I have long said that bathrooms should be a social space, and even in a single-occupancy restroom, the opportunity to invite friends along is an amazing opportunity. It also allows easy rest, redress, etc.</p>
+
+    <p>Next, though the 70’s aesthetic may have been wiped away, you can’t never forget your roots. A single ceiling panel directly above the toilet is a draw-dropping office beige, and sitting on the toilet, I smiled up for about three minutes straight at it.</p>
+
+    <p>The sink is the final thing we need to talk about, and boy are there things to talk about. Look at this sink. Normal, right? “Of course!” you think to yourself. “That’s just a normal Aqualyn(c) drop-in sink from American Standard! What’s the problem!” you guffaw.</p>
+
+    [image]
+
+    <p>You’re focused on the wrong thing, non-descript gender reader. Look at what the sink rests upon:</p>
+
+    <p>It was surprisingly easy to crack this puppy open. I’m not going to tell you though, it’s kinda my little secret place to store snacks now :)</p>
+
+    <p>This restroom overall is mediocre. I really wish I could say more positive things about it, but it just didn’t tickle my fancy</p>
+
+    <p>VERIDCT: 2/5 plungers</p>
+
+    <p>* which I do believe changed the design of the restrooms, based on cross-checking the design of this restroom with designs of similar restrooms modeled in the 2000s</p>
+  </article>
+</main>
+
+<style>
+  h1 {
+    font-size: 2em;
+  }
+
+  article {
+    width: min(65ch, 95vw)
+  }
+
+  p.articleContent {
+    font-weight: 500;
+    font-size: 1.2em 
+  }
+
+  img {
+    margin-bottom: 3ch;
+  }
+  p {
+    font-family: "miller-display", serif;
+    font-weight: 300;
+    font-size: 21px;
+
+    line-height: 1.5em;
+    margin-bottom: 1em;
+  }
+  p:last-child {
+    margin-bottom: 4ch;
+  }
+</style>
