50361bf6213c32f9853399b6aa72a58d0457ec03
mattsoza <mattsoza@protonmail.com>
2024-01-10 21:29:47 -0500

    Finalized restructure to have correct styles for each page

---
+++ src/routes/articles/+layout.svelte
@@ -0,0 +1,44 @@
+<!-- Scripting :) -->
+<script>
+	import BathroomFooter from "/src/components/bathroomFooter.svelte";
+  import Bathroomheader from "/src/components/bathroomheader.svelte";
+</script>
+
+
+
+<!-- HTML of nav and stylesheet -->
+<Bathroomheader />
+<slot />
+<BathroomFooter />
+
+
+<!-- Styling -->
+<style>
+:global(body) {
+	font-family: 'neue-haas-grotesk-display', sans-serif;
+	font-weight: 600;
+	font-style: normal;
+	margin: 0;
+
+  background-color: #FFFDF9;
+  min-height: 100vh;
+}
+
+:global(html, body) { 
+  min-height: 100vh;
+}
+
+:global(div > footer) {
+  position: sticky;
+  top: 100vh;
+}
+
+:global(main) {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+	padding-left: 5vw;
+  padding-right: 5vw;
+}
+
+</style>
